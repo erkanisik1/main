60a108cc4b3b69517e72f61589dce1a6f245fc40
Erkan IŞIK <erkanisik@yahoo.com>
2025-02-14 23:10:06 +0300

    Update actions.py

--- server/web/nginx/actions.py
+++ server/web/nginx/actions.py
@@ -21,39 +21,17 @@ ERROR_LOG    = '%s/error.log' % LOG_DIR
 ACCESS_LOG   = '%s/access.log' % LOG_DIR
 
 def setup():
-    autotools.rawConfigure("--user=nginx \
-                            --group=nginx \
-                            --prefix=%(html)s \
-                            --sbin-path=/usr/sbin/nginx \
-                            --conf-path=%(conf)s \
-                            --pid-path=%(pid)s \
-                            --lock-path=%(lock)s \
-                            --error-log-path=%(error)s \
-                            --http-log-path=%(access)s \
-                            --http-client-body-temp-path=%(home)s/client_body \
-                            --http-proxy-temp-path=%(home)s/proxy \
-                            --http-fastcgi-temp-path=%(home)s/fastcgi \
-                            --with-ipv6 \
+   
+
+    autotools.rawConfigure("--prefix=/usr/local/nginx \
+                            --sbin-path=/usr/local/nginx/sbin/nginx \
+                            --conf-path=/etc/nginx/nginx.conf \
+                            --pid-path=/run/nginx.pid \
+                            --lock-path=/run/nginx.lock \
+                            --error-log-path=/var/log/nginx/error.log \
+                            --http-log-path=/var/log/nginx/access.log \
                             --with-http_ssl_module \
-                            --with-http_realip_module \
-                            --with-http_addition_module \
-                            --with-http_xslt_module \
-                            --with-http_image_filter_module \
-                            --with-http_geoip_module \
-                            --with-http_sub_module \
-                            --with-http_dav_module \
-                            --with-http_flv_module \
-                            --with-http_gzip_static_module \
-                            --with-http_stub_status_module \
-                            --with-http_perl_module \
-                            --with-mail \
-                            --with-mail_ssl_module" % {'html': NGINX_HTML, \
-                                                       'conf': NGINX_CONF, \
-                                                       'pid': NGINX_PID, \
-                                                       'lock': NGINX_LOCK, \
-                                                       'error': ERROR_LOG, \
-                                                       'access': ACCESS_LOG, \
-                                                       'home': NGINX_HOME})
+                            --with-http_perl_module})
 
 
 def build():
